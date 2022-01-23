2fa8e59b566340af438d45521d543ec84a6a5515
Hamel Barrero <hamel_-barrero@hotmail.com>
2022-01-23 10:02:29 -0500

    config css basic

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,51 @@
 <script setup></script>
 
 <template>
-  <router-view />
+  <div class="wrapper">
+    <main class="main container">
+      <router-view />
+    </main>
+  </div>
 </template>
+
+<style>
+  :root {
+    --background-color: #fff;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    text-decoration: none;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: var(--background-color);
+    color: #333;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 16px;
+  }
+
+  .container {
+    width: 85%;
+    margin: 0 auto;
+  }
+
+  .wrapper {
+    display: grid;
+    grid-template-areas:
+      'navbar'
+      'main';
+    grid-template-rows: 3rem 1fr;
+    grid-template-columns: 1fr;
+    height: 100vh;
+  }
+
+  .main {
+    grid-area: main;
+    overflow: auto;
+  }
+</style>
